b960ccbac507067e9d303606d4a5dae4f5dcbf69
shruti2999 <shrutikumari9208@gmail.com>
2025-04-13 17:23:55 +0530

    update

--- src/Components/Layouts/Header.jsx
+++ src/Components/Layouts/Header.jsx
@@ -36,131 +36,126 @@ const Header = () => {
 
   return (
     <>
-      <nav className="bg-gray-900  border-gray-200">
-        <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
-          <Link
+      <nav className="bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-600 border-gray-200">
+  <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
+    <Link to="/" className="rtl:space-x-reverse flex items-center space-x-3">
+      <img src={logo} className="h-16 w-16 rounded-2xl" alt="Logo" />
+      <span className="whitespace-nowrap text-white self-center text-2xl font-semibold">
+        Medicart
+      </span>
+    </Link>
+    <Searchinput />
+    <button
+      onClick={toggleMenu}
+      type="button"
+      className="md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 dark:text-gray-400 focus:ring-gray-600 inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg"
+      aria-controls="navbar-default"
+      aria-expanded={isMenuOpen ? "true" : "false"}
+    >
+      <span className="sr-only">Open main menu</span>
+      <svg
+        className="w-5 h-5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 17 14"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M1 1h15M1 7h15M1 13h15"
+        />
+      </svg>
+    </button>
+    <div
+      className={`w-full md:block md:w-auto ${isMenuOpen ? "block" : "hidden"}`}
+      id="navbar-default"
+    >
+      <ul className="md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-600 md:bg-transparent border-gray-700 flex flex-col p-4 mt-4 font-medium border rounded-lg">
+        <li>
+          <NavLink
             to="/"
-            className="rtl:space-x-reverse flex items-center space-x-3"
+            className="md:border-0 md:p-0 text-white hover:text-blue-500 hover:bg-blue-700 rounded px-3 py-2 block"
+            aria-current="page"
           >
-            <img src={logo} className="h-16 w-16 rounded-2xl" alt="Logo" />
-            <span className="whitespace-nowrap text-white self-center text-2xl font-semibold">
-              MedPlus
-            </span>
-          </Link>
-          <Searchinput />
-          <button
-            onClick={toggleMenu}
-            type="button"
-            className="md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2  dark:text-gray-400  focus:ring-gray-600 inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg"
-            aria-controls="navbar-default"
-            aria-expanded={isMenuOpen ? "true" : "false"}
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className="md:border-0 md:p-0 text-white hover:text-blue-500 hover:bg-blue-700 rounded px-3 py-2 block"
           >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <div
-            className={`w-full md:block md:w-auto ${
-              isMenuOpen ? "block" : "hidden"
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={`/dashboard/${
+              auth?.user?.userType === "admin" ? "admin" : "user"
             }`}
-            id="navbar-default"
+            className="md:border-0 md:p-0 text-white hover:text-blue-500 hover:bg-blue-700 rounded px-3 py-2 block"
           >
-            <ul className="md:p-0  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  bg-gray-800 md:bg-gray-900 border-gray-700 flex flex-col p-4 mt-4 font-medium border rounded-lg">
-              <li>
-                <NavLink
-                  to="/"
-                  className=" md:border-0  md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 dark:hover:text-white md:hover:bg-transparent block px-3 py-2  rounded"
-                  aria-current="page"
-                >
-                   
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/about"
-                  className=" md:border-0  md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent block px-3 py-2  rounded"
-                >
-                  About
-                </NavLink> 
-              </li>
-              <li>
-                <NavLink
-                  to={`/dashboard/${
-                    auth?.user?.userType === "admin" ? "admin" : "user"
-                  }`}
-                  className=" md:border-0  md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent block px-3 py-2  rounded"
-                >
-                  {auth?.user?.userType === "admin" ? "DashBoard" : "Orders"}
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/contact-us"
-                  className=" md:border-0  md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent block px-3 py-2  rounded"
-                >
-                  Contact Us
-                </NavLink>
-              </li>
-              <li>
-                <Badge count={cart?.length} showZero>
-                  <NavLink
-                    to="/cart"
-                    className="hover:bg-gray-100 md:border-0 md:p-0 text-white md:hover:text-blue-500 dark:hover:bg-gray-700 hover:text-white md:hover:bg-transparent block px-3 py-2  rounded"
-                  >
-                    <FaShoppingCart className="text-2xl" title="Cart "/>
-                  </NavLink>
-                </Badge>
-              </li>
-              {!auth.user ? (
-                <>
-                  <li>
-                    <NavLink
-                      to="/log-in"
-                      className=" md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent block px-3 py-2  rounded"
-                    >
-                      <TbLogin2 className="text-2xl" title="Log In"/>
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/sign-up"
-                      className=" md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent block px-3 py-2  rounded"
-                    >
-                      <FaUserPlus className="text-2xl" title="Sign Up"/>
-                    </NavLink>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li>
-                    <NavLink
-                      onClick={handleLogout}
-                      to="/log-in"
-                      className="hover:bg-gray-100 md:border-0 md:p-0 text-white md:hover:text-blue-500 dark:hover:bg-gray-700 hover:text-white md:hover:bg-transparent block px-3 py-2  rounded"
-                    >
-                      <TbLogout2  className="text-2xl" title="Log Out"/>
-                    </NavLink>
-                  </li>
-                </>
-              )}
-            </ul>
-          </div>
-        </div>
-      </nav>
+            {auth?.user?.userType === "admin" ? "DashBoard" : "Orders"}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/contact-us"
+            className="md:border-0 md:p-0 text-white hover:text-blue-500 hover:bg-blue-700 rounded px-3 py-2 block"
+          >
+            Contact Us
+          </NavLink>
+        </li>
+        <li>
+          <Badge count={cart?.length} showZero>
+            <NavLink
+              to="/cart"
+              className="hover:bg-gray-100 md:border-0 md:p-0 text-white md:hover:text-blue-500 dark:hover:bg-gray-700 hover:text-white md:hover:bg-transparent block px-3 py-2 rounded"
+            >
+              <FaShoppingCart className="text-2xl" title="Cart " />
+            </NavLink>
+          </Badge>
+        </li>
+        {!auth.user ? (
+          <>
+            <li>
+              <NavLink
+                to="/log-in"
+                className="md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-blue-700 hover:text-white md:hover:bg-transparent block px-3 py-2 rounded"
+              >
+                <TbLogin2 className="text-2xl" title="Log In" />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/sign-up"
+                className="md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-blue-700 hover:text-white md:hover:bg-transparent block px-3 py-2 rounded"
+              >
+                <FaUserPlus className="text-2xl" title="Sign Up" />
+              </NavLink>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink
+                onClick={handleLogout}
+                to="/log-in"
+                className="hover:bg-gray-100 md:border-0 md:p-0 text-white md:hover:text-blue-500 dark:hover:bg-gray-700 hover:text-white md:hover:bg-transparent block px-3 py-2 rounded"
+              >
+                <TbLogout2 className="text-2xl" title="Log Out" />
+              </NavLink>
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
+  </div>
+</nav>
+
     </>
   );
 };
--- src/Components/UI/Carousal.jsx
+++ src/Components/UI/Carousal.jsx
@@ -4,20 +4,21 @@ import { RxDotFilled } from 'react-icons/rx';
 
 const Carousal = () => {
   const slides = [
+    
     {
-      url: "https://images.pexels.com/photos/3652097/pexels-photo-3652097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      url: 'https://media-hosting.imagekit.io/065b25be40144c99/Screenshot%202025-04-13%20165808.png?Expires=1839151759&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=qb9cwtmncrXrO929P4jlSP1zXDyvUl0BfRhqKViW1pzKm36B1gq-weF6d6dkaIcevE9rpW7Y2zUFcl0NSgvP-jTd1lqwl3jsesvj5VmWB3q-CeZ6mj0xdHlFl4FJ-j8ZZCiKpSoTkOfkZpAinhPv2EdVJYeWxzS3GfOOu6evdgcbZuhMuKZkzAsMP9Jdv874SjBYjB9GRN43muocEvhrd7-ln1GNehJ8blQPPpOfkZpGdBVgOCqbqpd8Q14HshDzZiSgokPsQTwBtYPWuzr~g5KA~iklsH8U2yoM-7BX4oHX4a7hiISZbWc004-GNipkL3~FER1maKpspYcIaa3Ijg__',
     },
     {
-      url: 'https://images.pexels.com/photos/40568/medical-appointment-doctor-healthcare-40568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      url: 'https://media-hosting.imagekit.io/8ce2339b50af48a2/Screenshot%202025-04-13%20165154.png?Expires=1839151333&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=c~2ROy0--W9si7gI5KLbZkc5ndtvkE2vBcLRHgrllZdJFsL8CRP2OK1jigubUQSbzJTPazjFajDIJoorsOzK0FG9qu~9gywEWPWR3qh5Px3vCTQ3JCl8Sqko2Qvt9PtAtKcP~wqukiqRVTewEjoC7FPfqeAnMy4bPRMLYK92PoWUdfpy1lmlVcptVdrEp-4wzR61fpcGmRgSdWnFtgTs6SPJx1Ry1Gm~Nxyw9EESNPz8tk3Di1DPQIz34YksU6IM1e7TbYB5Z6ELjVz6dGTRhRzgX~KSRLCqSHZ-n1q0QzN~s4OZ6j3f~ho5cqOBmzl3vO4pMJjhY~HGLYBhrtGNyA__',
     },
     {
-      url: 'https://images.pexels.com/photos/139398/thermometer-headache-pain-pills-139398.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      url: 'https://media-hosting.imagekit.io/ecc5cc99eec04533/Screenshot%202025-04-13%20165408.png?Expires=1839151480&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=W~g-~dz8r9yWFsKGq0V8-XTsYi0JPbnoZ~X8PHmYgATgusUp~rqPuAkVxc2t6C13CL7y9xvRxkeO3ByrYaBsM4MORTzEuv8QOQMvPNN5pd6asBBbrPQkCi-VPxVT1Su1MfSGE3bYMf8HmWuDt5q-LKSjvZeQ4EwqVQaEgBKcD39ghwnC3ruDcPerppHzcMRr05IlqN1N9Nc8T9PZLIDh0wNtv6IGIz6PSRWIbR-2mtmmwj5I4Fu91u7ivdI4qXzbh5EUs85zhJu9rdGj8z4OwykLialFNp4p6h1Y6nkjElNLtD1-kX~es2pa1OsgWUYi~W0DawEfDKDczXU9btwYlA__',
     },
     {
-      url: 'https://images.pexels.com/photos/5327584/pexels-photo-5327584.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      url: 'https://media-hosting.imagekit.io/cc04075a017f4177/Screenshot%202025-04-13%20165431.png?Expires=1839151574&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=oAMW7XG2XQ6Ad4bL1gWyMcP0-BArWOviX2ScJ1nSh3jgAazI7~ZqypNiHlo3EUvv89yHvOAaLJoM1NZF4~xuM11CTZkFbrRgvwmTqTTC2B~vTokcJV3QBNptEQc7mOw1dU0uYqvf7BoIY9-q2HFy1iCm~Uk7Xe7jhaSTc6q~AbnLtGnLf2mLt3wy4q4QfJQcV5Azq~ecY~NOIl6pt7ZK2wG~9oFUj-neAvX4WhDpH~27wgBPJOA-dw3ZYxYnlHhedIA-RcUbQW9Dzki2Dgy39nLJd1tG6GPX1SPH8-8H9Z~iqphejOaIAhz0LPPstrUINY77wBkqKNJoOqg8XK4jKw__',
     },
     {
-      url: 'https://images.pexels.com/photos/4021819/pexels-photo-4021819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      url: 'https://media-hosting.imagekit.io/9fc3ee04ec4c42fb/Screenshot%202025-04-13%20165749.png?Expires=1839151714&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=g5wbh8aV6V32zRXrs8NNyP8WaYKhwD3YNfOwlExzmkal2POvXq6a1LbxYZnWD2B6c3TrUJwnPw1xNtMKPRpWAqFi1SUY9vCeYLC5lboTsc05QUijf3sW1WVDe2yABzeSsjSddpll6kfEljKFf7GvL17YWeUEIFZyyzLq1RE3zykGlshTZjrgJXJHzT-TXerN55h93WsILTfzS6ZULl-rPRncXs7TqQt9IsxMSBsgw3F-yh8ZrBJA56gIXKqYI1Roy9qKuJ3OFr9CyP6ME2xovzDjDb6zJnvJ~Fxxg5oxHRLuGxVQOS2S0aLn1n5IesIOtQf7exJUhRVTW7yyJzKwNA__',
     },
   ];
 
